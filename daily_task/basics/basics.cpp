e7e1a3cd12ff215581ba64bfab7f0b8bda19e7cf
Dipen Ranjitsinh Padhiyar <37871638+dipenpadhiyar@users.noreply.github.com>
2023-06-08 16:00:05 +0530

    8-06-23

--- daily_task/basics/basics.cpp
+++ daily_task/basics/basics.cpp
@@ -27,22 +27,3 @@ int main() {
   return 0;
   
 }
-
-
-
-#include <bits/stdc++.h>  // This will work only for g++ compiler.
-#define fast ios::sync_with_stdio(false);cin.tie(0)
-#define ll long long 
-using namespace std;
-
-
-
-int main() {
-    fast;
-    ll t=1;cin>>t;
-    while(t--){
-        cout<<"Hello";
-    }
-    
-    return 0;
-}
