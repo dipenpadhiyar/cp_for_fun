bd1460a872f3a29781444859b2db4c62776e1b59
Dipen Ranjitsinh Padhiyar <37871638+dipenpadhiyar@users.noreply.github.com>
2023-06-16 12:49:08 +0530

    "update_16-06"

---
+++ contest/Leetcode/344/344-reverse-string.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>  // This will work only for g++ compiler.
+#define fast ios::sync_with_stdio(false);cin.tie(0)
+#define ll long long 
+using namespace std;
+
+#define tab "\t"
+int main() {
+    fast;
+    vector<char> s={'d','i'};
+    for(char i : s){
+        cout<<tab << i;
+    }
+    return 0;
+}
---
+++ contest/Leetcode/541-reverse-string-ii.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=541 lang=cpp
+ *
+ * [541] Reverse String II
+ */
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(false);cin.tie(0)
+#define tab "\t"
+int main(){
+    fast;
+    string s = "abcdefg";
+    int s_size = s.size();
+    int k = 2;
+    string temp;
+    // algo reverser first K() caracter
+    reverse(s.begin(),s.begin()+k);
+    for(i=k;i<s_size;i+(2k)){
+    }
+
+    //update position of k with two
+    k = ;
+    if(){
+    }
+    cout<<s;
+    return 0;
+}
+
