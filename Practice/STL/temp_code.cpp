ee04da6aebcea8a74f9b2c4f70f2a8e5052e0ca8
Dipen Padhiyar <37871638+dipenpadhiyar@users.noreply.github.com>
2023-07-30 16:34:42 +0000

    30-july

--- Practice/STL/temp_code.cpp
+++ Practice/STL/temp_code.cpp
@@ -2,11 +2,11 @@
 #define fast ios::sync_with_stdio(false);cin.tie(0)
 #define ll long long 
 using namespace std;
-
-bool x(int d1,int d3){
-    return d1 > d3;
-}
-
+//comparator function
+    bool x(int d1,int d3){
+        return d1 > d3;
+    }
+//////////////////////////////////// VECTOR//////////////////////////////////// 
 void vectorDemo(){
     vector<int> data = {11,3,4,14};
 
@@ -37,6 +37,7 @@ void vectorDemo(){
     cout << it2 - it << endl;
 
     ////////sort the data with sort opeeration
+    
     sort(data.begin(),data.end() , x);
      for(auto i : data){
         cout<< i << "\t";
@@ -50,12 +51,57 @@ void vectorDemo(){
 
 }
 
+//////////////////////////////////// SET ////////////////////////////////////
 void setDemo(){
+    ////each option in (log N) time in set./////
     set<int> s;
-    
+    s.insert(5);
+    s.insert(4);
+    s.insert(3);
+    s.insert(2);
+    s.insert(1);
+    s.insert(-5);
+    s.insert(-10);
+    for(auto i : s){
+        cout << i <<  "\t";
+    }
+    auto it = s.find(-10);
+    if(it == s.end()){
+        cout << "element not present. \n";
+    }else{
+        cout << "element present \n";
+    }
+cl
+    auto it2  = s.lower_bound(-5);
+    auto it3 = s.upper_bound(-5);
+
+    cout << *it2 << " " << *it3  << endl;
+}
+
+//////////////////////////////////// MAP ////////////////////////////////////
+void mapDemo(){
+    map<int, int > data;
+    data[1] = 100;
+    data[2] = 1;
+    data[4] = 1000;
+    data[3] = 10;
+    ////////print map///
+    for(auto i : data){
+        cout <<  " key value is : " << i.first << " and value holds : " << i.second << endl;
+    }
+    //////////////////char print
+    auto it = data.begin();
+    map<char, int> cti;
+    string s = "dipen padhiyar";
+    for(auto c : s){
+        cti[c]++;
+    }
+   
+
+
 }
 int main() {
     fast;
-    
+    mapDemo();
      return 0;
 }
